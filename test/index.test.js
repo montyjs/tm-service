5b4e8ed96efaa1ce414ee0f8fea46482c293dfbf
Trevor Markel <tmarkel6849@gmail.com>
2019-05-20 11:16:38 -0700

    updated circlci config, created async server test

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,16 @@
 /* eslint-disable no-undef */
+const rp = require('request-promise');
 const functions = require('./index.js');
+const { getAll } = require('../database/index.js');
+
 
 test('should return 4 for adding 2 + 2', () => {
   expect(functions.add(2, 2)).toBe(4);
+});
+
+test('server should retrieve and serve data from database', () => {
+  expect.assertions(1);
+  return rp('http://localhost:3002/reviews').then((response) => {
+    expect(response).toBeDefined();
+  });
 });
