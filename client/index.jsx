7a351c7c4d27fbdbbe10c46d3df4cc1be222dce5
Trevor Markel <tmarkel6849@gmail.com>
2019-05-20 20:37:29 -0700

    created main scss style file, imported @ index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -2,5 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
+import style from './main.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
