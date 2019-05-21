76b7e9273ab5884eafe8d60ab69061361321a8af
Trevor Markel <tmarkel6849@gmail.com>
2019-05-21 12:20:48 -0700

    added all preliminary divs, stylesheet working

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -29,13 +29,50 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.state.reviews.map(entry => (
-          <Reviews review={entry} />
-        ))}
+      <div className="top-box">
+        <div className="review-box">
+          <div className="top-bar">
+            <div className="stars">
+              <div className="star-1 allstar">★</div>
+              <div className="star-2 allstar">★</div>
+              <div className="star-3 allstar">★</div>
+              <div className="star-4 allstar">★</div>
+              <div className="star-5 allstar">★</div>
+            </div>
+            <div className="username">Username</div>
+            <div className="postdate">6 months ago</div>
+          </div>
+          <div className="title">
+            <div className="title-content">First pair of shoes</div>
+          </div>
+          <div className="review">
+            <div className="reviewContent">
+              These guys are pretty solid.They stick well, they're true to Sportiva sizing and they feel fine on my feet.
+              Not super precise, though, and the rubber is so thick, blunt and flat that it takes some time to learn to trust them.
+              Especially if you're coming off an aggressive shoe.
+            </div>
+          </div>
+          <div className="review-footer">
+            <div className="recommended">✔ Yes, I recommend this product</div>
+            <div className="helpfull">Helpfull?</div>
+            <button type="button" className="button-yes">Yes</button>
+            <button type="button" className="button-no">No</button>
+            <button type="button" className="button-report">Report as inappropriate</button>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+// {
+//   this.state.reviews.map(entry => ( <
+//     Reviews review = {
+//       entry
+//     }
+//     />
+//   ))
+// }
--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 import style from './main.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const Index = () => (
+  <div>
+    <App />
+  </div>
+);
+
+ReactDOM.render(<Index />, document.getElementById('root'));
