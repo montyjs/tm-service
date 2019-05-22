dd525e992a8a4ecb755d9f204e4905838e91f41f
Trevor Markel <tmarkel6849@gmail.com>
2019-05-22 13:32:46 -0700

    updated Reviews component, rendoring correctly with stars filled correctly

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -5,12 +5,47 @@
 import React from 'react';
 
 const Reviews = (props) => (
-  <div>
-    <div>
-      Posted on Data: {props.review.postdate}
+  <div className="top-level">
+    <div className="content-header">
+      <div className="stars">
+        <span style={props.starColor(props.review.rating, 1)} className="star-1">★</span>
+        <span style={props.starColor(props.review.rating, 2)} className="star-2">★</span>
+        <span style={props.starColor(props.review.rating, 3)} className="star-3">★</span>
+        <span style={props.starColor(props.review.rating, 4)} className="star-4">★</span>
+        <span style={props.starColor(props.review.rating, 5)} className="star-5">★</span>
+      </div>
+      <div className="author">
+        {props.review.username}
+        {console.log(props.review.rating)}
+      </div>
+      <div className="timestamp">
+        · {props.review.postdate}
+      </div>
     </div>
-    <div>
-      Review Tite: {props.review.title}
+    <div className="content-title">
+      {props.review.title}
+    </div>
+    <div className="content-body">
+      <div className="review-data">
+        {props.review.review}
+      </div>
+      <div className="recommended">
+        <p>
+          <span className="check">✔</span>
+          <span className="yes"> Yes</span>
+          , I recommend this product
+        </p>
+      </div>
+    </div>
+    <div className="content-footer">
+      <div className="helpful">
+        Helpful?
+      </div>
+      <div className="buttons">
+        <button type="button" className="btn-yes">Yes · 25</button>
+        <button type="button" className="btn-no">No · 25</button>
+        <button type="button" className="btn-report">Report as inappropriate</button>
+      </div>
     </div>
   </div>
 );
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -29,8 +29,8 @@ class App extends React.Component {
       });
   }
 
-  starColor(num) {
-    if (this.count >= num) {
+  starColor(num, star) {
+    if (num >= star) {
       return { color: '#bd5b0d' };
     }
     return {};
@@ -38,52 +38,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="top-level">
-        <div className="content-header">
-          <div className="stars">
-            <span style={this.starColor(1)} className="star-1">★</span>
-            <span style={this.starColor(2)} className="star-2">★</span>
-            <span style={this.starColor(3)} className="star-3">★</span>
-            <span style={this.starColor(4)} className="star-4">★</span>
-            <span style={this.starColor(5)} className="star-5">★</span>
-          </div>
-          <div className="username">
-            Bobby Joe
-          </div>
-          <div className="timestamp">
-            · 7 years ago
-          </div>
-        </div>
-        <div className="content-title">
-          This is my review
-        </div>
-        <div className="content-body">
-          <div className="review-data">
-            Great shoes right out of the box.Spent 5 hours clmbing in them the day after I bought them and the performed great.
-            They were able to hold small toe holds and slick smears(which is what I was looking for).
-            They started forming to my feet after the first couple of routes, but did not get flimsy.
-            I would recommend these to anyone contimplating the Mythos or similar level shoes, as I was keeping up with and finishing things my budy wearing those were not.
-            I had also looked at those shoes but decided that the $60 price difference was worth the shot.
-            Still will problably end up with them in the future, but these are great shoes to grow with.
-          </div>
-          <div className="recommended">
-            <p>
-              <span className="check">✔</span>
-              <span className="yes"> Yes</span>
-              , I recommend this product
-            </p>
-          </div>
-        </div>
-        <div className="content-footer">
-          <div className="helpful">
-            Helpful?
-          </div>
-          <div className="buttons">
-            <button type="button" className="btn-yes">Yes · 25</button>
-            <button type="button" className="btn-no">No · 25</button>
-            <button type="button" className="btn-report">Report as inappropriate</button>
-          </div>
-        </div>
+      <div className="parent-level">
+        {this.state.reviews.map(review => (
+          <Reviews review={review} starColor={this.starColor} />
+        ))}
       </div>
     );
   }
