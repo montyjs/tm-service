61c70703e6761832904844a2be023862c02a80dc
Trevor Markel <tmarkel6849@gmail.com>
2019-05-23 12:12:50 -0700

    created jest config and installed babel-jest

---
+++ jest-preset.js
@@ -0,0 +1,22 @@
+module.exports = {
+  automock: false,
+  moduleDirectories: [
+    'node_modules',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTest.js'],
+  transform: {
+    '\\.js$': 'babel-jest',
+  },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  verbose: true,
+};
