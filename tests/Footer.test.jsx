90a9a03ee59842bd79a80838792fb2591cc06753
Trevor Markel <tmarkel6849@gmail.com>
2019-05-24 09:18:41 -0700

    created button tests for Footer components

--- tests/Footer.test.jsx
+++ tests/Footer.test.jsx
@@ -10,3 +10,25 @@ describe('Should render', () => {
     expect(wrapper);
   });
 });
+
+describe('Should accept a prop to execute for click events', () => {
+
+  const clickMock = jest.fn();
+  const clickWrapper = shallow(<Footer upVote={clickMock} />);
+  const event = { target: { innerText: 'some text' } };
+
+  it('should have a report button', () => {
+    clickWrapper.find('.btn-report').simulate('click');
+    expect(clickMock.call.length).toEqual(1);
+  });
+
+  it('should call the passed function when yes button is clicked', () => {
+    clickWrapper.find('.btn-yes').simulate('click', event);
+    expect(clickMock.call.length).toEqual(1);
+  });
+
+  it('should call the passed function when no button is clicked', () => {
+    clickWrapper.find('.btn-no').simulate('click', event);
+    expect(clickMock.call.length).toEqual(1);
+  });
+});
