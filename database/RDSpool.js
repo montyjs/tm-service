6f1f0d7a0acdc0895a8a93c4471313075cfc24e3
Trevor Markel <tmarkel6849@gmail.com>
2019-05-31 21:33:09 -0600

    created new pool with new rds db

--- database/RDSpool.js
+++ database/RDSpool.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const RDSpool = new Pool({
-  connectionString: process.env.DB_URI,
+  connectionString: 'postgresql://rei:batman123@reviewdb.c6cy0b3pfl6c.us-west-2.rds.amazonaws.com:5432/reviews',
 });
 
 module.exports.RDSpool = RDSpool;
--- database/index.js
+++ database/index.js
@@ -1,23 +1,24 @@
 /* eslint-disable no-console */
 require('dotenv').config();
 const { Pool } = require('pg');
+const { RDSpool } = require('./RDSpool');
 
-const pool = process.env.NODE_ENV === 'production'
-  ? new Pool({
-    connectionString: process.env.DB_URI,
-  })
-  : new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: 'reireviews',
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
+const pool = new Pool({
+  connectionString: process.env.DB_URI,
+});
+  // : new Pool({
+  //   user: process.env.DB_USER,
+  //   host: process.env.DB_HOST,
+  //   database: 'reireviews',
+  //   password: process.env.DB_PASSWORD,
+  //   port: process.env.DB_PORT,
+  // });
 
 const getAll = (cb) => {
   console.log('request at database, retrieving data');
+  console.log('current NODE_ENV', process.env.NODE_ENV);
   const queryString = 'SELECT * FROM reviews';
-  pool.query(queryString, (err, result) => {
+  RDSpool.query(queryString, (err, result) => {
     if (err) {
       return console.error(err.message);
     }
