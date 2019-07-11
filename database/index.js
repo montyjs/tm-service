f0a1259f210fe2ecb8ba56fef19c547f6f6cfe8f
Trevor Markel <tmarkel6849@gmail.com>
2019-07-11 14:23:20 -0600

    cleaned up server and db index files, added comments

--- database/index.js
+++ database/index.js
@@ -1,31 +1,37 @@
 /* eslint-disable no-console */
-require('dotenv').config();
-const { Pool } = require('pg');
+require('dotenv').config()
+const { Pool } = require('pg')
 
-const pool = new Pool({
-  connectionString: process.env.DB_URI,
-});
-  // : new Pool({
-  //   user: process.env.DB_USER,
-  //   host: process.env.DB_HOST,
-  //   database: 'reireviews',
-  //   password: process.env.DB_PASSWORD,
-  //   port: process.env.DB_PORT,
-  // });
+/******************* DATABASE CONNECTION *********************/
+
+const pool = process.env.NODE_ENV === 'production'
+  ? new Pool({
+    connectionString: process.env.DB_URI
+  })
+  : new Pool({
+    user: process.env.LOCAL_USER,
+    host: process.env.LOCAL_HOST,
+    database: 'reireviews',
+    password: process.env.LOCAL_PASSWORD,
+    port: process.env.LOCAL_PORT,
+  })
+
+/******************* ACCESSING DATA ***********************/
 
 const getAll = (cb) => {
-  console.log('request at database, retrieving data');
-  console.log('current NODE_ENV', process.env.NODE_ENV);
-  console.log('current DB_URI', process.env.DB_URI);
-  const queryString = 'SELECT * FROM reviews';
+  const queryString = 'SELECT * FROM reviews'
   pool.query(queryString, (err, result) => {
     if (err) {
-      return console.error(err.message);
+      return console.error(err.message)
     }
-    console.log('results received, sending to callback now');
-    return cb(result.rows);
-  });
-};
+    console.log('results received, sending to callback now')
+    return cb(result.rows)
+  })
+}
 
-module.exports.pool = pool;
-module.exports.getAll = getAll;
+/******************** EXPORTS *******************/
+
+module.exports = {
+  pool,
+  getAll
+}
--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
 /* eslint-disable no-console */
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-const { getAll } = require('../database/index.js');
+require('dotenv').config()
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      path = require('path'),
+      cors = require('cors'),
+      db = require('../database/index.js')
 
-const app = express();
+const app = express(),
+      PORT = process.env.PORT || 3002
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, '/../public/dist')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.static(path.join(__dirname, '/../public/dist')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/********************* ROUTES ***********************/
 
 app.get('/reviews', (req, res) => {
-  console.log('request at server, heading to database');
-  getAll((result) => {
-    if (!result) {
-      res.sendStatus(400);
-    }
-    res.send(result);
-  });
-});
+  console.log('request at server, heading to database')
+  db.getAll((result) => {
+    !result ? res.sendStatus(400) : res.send(result)
+  })
+})
 
 app.get('/bundle', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/dist/bundle.js'));
-});
+  res.sendFile(path.join(__dirname, '../public/dist/bundle.js'))
+})
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
+/********************** SERVER CONNECTION *********************/
+
+app.listen(PORT, (err) => {
+  err ? console.log('error starting server: ', err) : console.log(`listening on port ${PORT}`)
+})
